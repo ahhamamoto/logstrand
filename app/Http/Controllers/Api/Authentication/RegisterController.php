928a8fc47b11988bac51a7b363ad1eff70b52109
Anderson Hiroshi Hamamoto <anderson.hamamoto@gmail.com>
2020-05-22 18:15:43 -0300

    User registration

---
+++ app/Http/Controllers/Api/Authentication/RegisterController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\Authentication;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Authentication\RegisterPostRequest;
+use App\Http\Resources\Api\Users\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function __invoke(RegisterPostRequest $request)
+    {
+        $array = ['someone'];
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return new UserResource($user);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::post('register', Api\Authentication\RegisterController::class);
     return $request->user();
 });
